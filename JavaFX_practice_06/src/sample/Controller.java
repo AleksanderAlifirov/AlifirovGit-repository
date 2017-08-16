2785ada14ed2ad4908a454f6d484695b5284bed5
Aleksandr Alifirov <aalifirov@gmail.com>
2017-08-16 17:13:21 +0700

    set color Blue

---
+++ JavaFX_practice_06/src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void firstButtonAction(ActionEvent actionEvent) {
+        System.out.println("Button1 pressed");
+    }
+
+    public void secondButtonAction(ActionEvent actionEvent) {
+        System.out.println("Button2 pressed");
+    }
+
+
+    public void thirdtButtonAction(ActionEvent actionEvent) {
+        System.out.println("Button3 pressed");
+    }
+}
