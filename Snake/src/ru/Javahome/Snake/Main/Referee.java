17a46ff0f07bad52f99b56c323821c6086212124
Aleksandr Alifirov <aalifirov@gmail.com>
2017-09-06 06:08:17 +0700

    Create check methods for winner

--- Snake/src/ru/Javahome/Snake/Main/Referee.java
+++ Snake/src/ru/Javahome/Snake/Main/Referee.java
@@ -4,8 +4,17 @@ public class Referee {
 
     public static boolean checkWin(char[][] field) {
         boolean win;
+        int count = 0;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++) {
+                if (field[i][j] == 'x') count++;
+            }
+        }
 
-        
         return win;
+    }
+
+    public boolean isAnyStringWin(char[][] field){
+        boolean
     }
 }
