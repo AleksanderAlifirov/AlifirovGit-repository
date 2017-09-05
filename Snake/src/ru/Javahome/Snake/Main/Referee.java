3f96af9abd58038907262182c76754e9725a972e
Aleksandr Alifirov <aalifirov@gmail.com>
2017-09-06 06:57:44 +0700

    Creat methods for checking rows and columns for win condition

--- Snake/src/ru/Javahome/Snake/Main/Referee.java
+++ Snake/src/ru/Javahome/Snake/Main/Referee.java
@@ -3,9 +3,88 @@ package ru.Javahome.Snake.Main;
 public class Referee {
 
     public static boolean checkWin(char[][] field) {
-        boolean win;
 
-        
+        int count;
+
+
+        return;
+    }
+
+    public boolean isAnyStringWin(char[][] field){
+        boolean win = false;
+        int countZero;
+        int countCross;
+        for (int i = 0; i < 3; i++){
+            countZero = 0;
+            countCross = 0;
+            for (int j = 0; j < 3; j++) {
+                if (field[i][j] == 'x') countCross++;
+                if (field[i][j] == 'o') countZero++;
+            }
+            if ((countZero == 3) || (countCross == 3)) {
+                win = true;
+                break;
+            }
+        }
+
+        return win;
+    }
+
+    public boolean isColumnWin (char[][] field){
+        boolean win = false;
+        int countZero;
+        int countCross;
+        for (int j = 0; j < 3; j++){
+            countZero = 0;
+            countCross = 0;
+            for (int i = 0; i < 3; i++) {
+                if (field[i][j] == 'x') countCross++;
+                if (field[i][j] == 'o') countZero++;
+            }
+            if ((countZero == 3) || (countCross == 3)) {
+                win = true;
+                break;
+            }
+        }
+        return win;
+    }
+
+    public boolean isDiagWin(char[][] field, String side){
+        boolean win = false;
+        int countZero;
+        int countCross;
+        switch(side){
+         case "right" :
+            for (int j = 0; j < 3; j++){
+                countZero = 0;
+                countCross = 0;
+                for (int i = 0; i < 3; i++) {
+                    if (field[i][i] == 'x') countCross++;
+                    if (field[i][i] == 'o') countZero++;
+                }
+                if ((countZero == 3) || (countCross == 3)) {
+                    win = true;
+                    break;
+                }
+            }
+            break;
+
+         case "left" :
+             for (int j = 0; j < 3; j++){
+                 countZero = 0;
+                 countCross = 0;
+                 for (int i = 0; i < 3; i++) {
+                     if (field[i][2 - i] == 'x') countCross++;
+                     if (field[i][2 - i] == 'o') countZero++;
+                 }
+                 if ((countZero == 3) || (countCross == 3)) {
+                     win = true;
+                     break;
+                 }
+             }
+
+        }
+
         return win;
     }
 }
