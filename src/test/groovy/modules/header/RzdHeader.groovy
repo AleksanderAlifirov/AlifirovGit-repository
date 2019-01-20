06a166be4b937bc51b00e75dd573d70a6a071895
Aleksandr Alifirov <aalifirov@gmail.com>
2019-01-20 18:10:38 +0700

    Checked headers references

--- src/test/groovy/modules/header/RzdHeader.groovy
+++ src/test/groovy/modules/header/RzdHeader.groovy
@@ -1,5 +1,6 @@
 package modules.header
 
+import org.openqa.selenium.By
 import rzd.RzdModule
 
 class RzdHeader extends RzdModule {
@@ -7,6 +8,10 @@ class RzdHeader extends RzdModule {
     static content = {
 
         О_Компании { $("a[href*='://']", text: 'О компании' )}
+        Пресс_Центр { find(By.xpath("//a[contains(text(), 'Пресс-центр')]")) }
+        Инвесторам { find(By.xpath("//a[contains(text(), 'Инвесторам')]")) }
+        Тендеры { find(By.xpath("//a[contains(text(), 'Тендеры')]")) }
+        Контакты { find(By.xpath("//a[contains(text(), 'Контакты')]")) }
     }
 
 }
--- src/test/groovy/specs/startPageSpecs/headerPartSpec/HeaderElementsSpec.groovy
+++ src/test/groovy/specs/startPageSpecs/headerPartSpec/HeaderElementsSpec.groovy
@@ -14,7 +14,14 @@ class HeaderElementsSpec extends RzdSpec {
         def view = at RzdStartPage
         then:
         view.Меню_Заголовка.О_Компании.displayed
-
         println "   Ссылка 'О компании' присутствует в заголовке."
+        view.Меню_Заголовка.Контакты.displayed
+        println "   Ссылка 'Контакты' присутствует в заголовке."
+        view.Меню_Заголовка.Тендеры.displayed
+        println "   Ссылка 'Тендеры' присутствует в заголовке."
+        view.Меню_Заголовка.Пресс_Центр.displayed
+        println "   Ссылка 'Пресс-центр' присутствует в заголовке."
+        view.Меню_Заголовка.Инвесторам.displayed
+        println "   Ссылка 'Инвесторам' присутствует в заголовке."
     }
 }
